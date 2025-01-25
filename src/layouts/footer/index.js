6597562a813702738fe2df82997f6de435a3fe3e
Chienlag1 <chienguyen17@gmail.com>
2025-01-25 19:24:51 +0700

    check profile

--- src/layouts/footer/index.js
+++ src/layouts/footer/index.js
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="text-sm leading-6">
       <div className="pb-28 border-t border-slate-200 sm:flex justify-between text-slate-500 dark:border-slate-200/5">
         <div className="mb-6 sm:mb-0 sm:flex">
-          <p>Copyright &copy;team 4ewgwer</p>
+          <p>Copyright &copy;team 1</p>
         </div>
       </div>
     </footer>
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import LoginScreen from "../pages/loginScreen";
 import VerifyScreen from "../pages/verifyScreen";
 import ForgetPassword from "../pages/forgetPasswordScreen";
 import NewPasswordScreen from "../pages/newPasswordScreen";
+import ProfileScreen from "../pages/profileScreen";
 
 const Routers = () => {
   const elements = useRoutes([
@@ -13,10 +14,10 @@ const Routers = () => {
       path: "/",
       element: <LayoutUser Page={HomeScreen}></LayoutUser>,
     },
-    // {
-    //   path: "/profile",
-    //   element: <PrivateRouterUser Page={() => <Layout Page={Profile} />} />, // Corrected component name
-    // },
+    {
+      path: "/profile",
+      element: <ProfileScreen />,
+    },
     {
       path: "/signup",
       element: <SignupScreen />,
